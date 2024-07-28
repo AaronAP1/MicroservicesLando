7f2f775dd9e1a3bfdadf63d6ed2f8d1ec4af8c33
AaronAP1 <aaalenortbless@gmail.com>
2024-07-27 21:53:32 -0500

    Test

---
+++ MicroLando/microservice-productostes/src/test/java/com/microservice/productostest/MicroserviceProductostesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.productostest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceProductostesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
